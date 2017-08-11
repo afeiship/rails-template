af73a45ccc801231256cb3091eb9adb836cd945c
afeiship <1290657123@qq.com>
2017-08-11 09:55:34 +0800

    issue: add debug gem

--- src/composer.rb
+++ src/composer.rb
@@ -1,2 +1,6 @@
 gsub_file "Gemfile", "https://rubygems.org", "https://ruby.taobao.org"
 gsub_file "Gemfile", "# gem 'bcrypt', '~> 3.1.7'", "gem 'bcrypt', '~> 3.1.7'"
+
+# gem debug:
+gem 'pry-rails', group: [:development, :test]
+gem 'pry-byebug', group: [:development, :test]
