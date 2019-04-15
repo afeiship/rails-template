7ea75266db1d5c9793906b3d43ba8a98dd9da024
afeiship <1290657123@qq.com>
2019-04-15 09:17:13 +0800

    fix: use rb-china

--- src/composer.rb
+++ src/composer.rb
@@ -1,6 +1,6 @@
-gsub_file "Gemfile", "https://rubygems.org", "https://ruby.taobao.org"
+gsub_file "Gemfile", "https://rubygems.org", "https://gems.ruby-china.com"
 gsub_file "Gemfile", "# gem 'bcrypt', '~> 3.1.7'", "gem 'bcrypt', '~> 3.1.7'"
 
 # gem debug:
-gem 'pry-rails', group: [:development, :test]
-gem 'pry-byebug', group: [:development, :test]
+# gem "pry-rails", group: [:development, :test]
+# gem "pry-byebug", group: [:development, :test]
